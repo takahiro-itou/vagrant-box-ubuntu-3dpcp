fc3024e9d9df41c1873e40797257c741fab22ed2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2023-06-25 10:02:32 +0900

    作成するマシン名とボックス名を変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash  -xue
 
-boxFile='ubuntu-gui.box'
-boxName='ubuntu-bionic64-gui'
+boxFile='ubuntu-3dpcp.box'
+boxName='ubuntu-focal64-3dpcp'
 
 # Account Name of Vagrant Cloud.
 vcUser='takahiro-itou'
